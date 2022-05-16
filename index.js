b085f1f4163c7f8f98adde26488bf1756ce94607
Ying Hu <36462094+yinghu-math@users.noreply.github.com>
2022-05-15 21:34:17 -0500

    switch statement, audio added when click the buttons.

--- index.js
+++ index.js
@@ -8,12 +8,50 @@ const buttons = document.querySelectorAll('.drum')
 }
 */
 
-function clicked(button){
-  alert(button.innerHTML+" button got clicked!");
+/* function clicked(button){
+  let crash = new Audio('sounds/crash.mp3');
+  crash.play()
 }
 
 for (const button of buttons) {
   button.addEventListener("click", function(){clicked(button)});
-}
+} */
 
-let crash = new Audio('sounds/crash.mp3');
+for (const button of buttons) {
+  button.addEventListener("click", function(){
+    // console.log(this); will give me the button that was triggered the function.
+    let buttonText = this.innerHTML;
+    switch (buttonText) {
+      case 'w':
+        let tom1 = new Audio('sounds/tom-1.mp3');
+        tom1.play();
+        break;
+      case 'a':
+        let tom2 = new Audio('sounds/tom-2.mp3');
+        tom2.play();
+        break;
+      case 's':
+        let tom3 = new Audio('sounds/tom-3.mp3');
+        tom3.play();
+        break;
+      case 'd':
+        let tom4 = new Audio('sounds/tom-4.mp3');
+        tom4.play();
+        break;
+      case 'j':
+        let snare = new Audio('sounds/snare.mp3');
+        snare.play();
+        break;
+      case 'k':
+        let crash = new Audio('sounds/crash.mp3');
+        crash.play();
+        break;
+      case 'l':
+        let kickBass = new Audio('sounds/kick-bass.mp3');
+        kickBass.play();
+        break;
+     default: 
+        console.log(buttonText);
+    }
+  });
+}
